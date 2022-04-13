26ea4b81c32f6ade5e2737b7bbcb48876c31f4db
Justin Lesh <jlesh56021@gmail.com>
2022-04-13 13:12:21 -0500

    Initial Commit. Example of working WorldWind projects.

---
+++ src/test.java
@@ -0,0 +1,11 @@
+import gov.nasa.worldwindx.examples.*;
+
+import javax.swing.*;
+
+public class test {
+    public static void main(String[] args) {
+        //ApplicationTemplate.start("WorldWind Bulk Download", BulkDownload.AppFrame.class);
+        //ApplicationTemplate.start("Shape Editing", ShapeEditing.AppFrame.class);
+        ApplicationTemplate.start("Ellipsoids", Ellipsoids.AppFrame.class);
+    }
+}
